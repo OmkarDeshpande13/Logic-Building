759b65eff4732b6ff78872dbde310fa56fdd56af
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-08-18 00:29:02 +0530

    calculate the sum of Non-factors

---
+++ Assignment4/program4.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+ int SumOFNonFactor(int iNo)
+ {
+   int iCnt = 0;
+   int iFrequency = 0;
+   if(iNo < 0)
+   {
+    iNo = -iNo;
+   }
+
+   for(iCnt = 1 ; iCnt <= iNo ; iCnt++)
+   {
+    if(iNo % iCnt != 0 )
+    {
+       iFrequency = iFrequency + iCnt;
+    }
+   }
+ return iFrequency;
+
+ } 
+
+int main()
+{
+  int iValue = 0;
+  int iRet = 0;
+
+  printf("Enter the Number");
+  scanf("%d",&iValue);
+
+  
+  iRet =  SumOFNonFactor(iValue);
+
+  printf("%d",iRet);
+
+    return 0;
+}
