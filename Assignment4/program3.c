352e3d33894242c20f6780979b63f68f03f02680
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-08-18 00:28:28 +0530

    Print the Non-Factors of a Number

---
+++ Assignment4/program3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+ 
+ int NonFactors(int iNo)
+ {
+     int iCnt = 0;
+
+     if(iNo<0)
+     {
+      iNo = -iNo;
+     }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+  {
+      if((iNo % iCnt)!=0)
+    {
+      printf("%d\n",iCnt);
+    }
+  }
+    
+ }
+  int main()
+  {
+    int iValue = 0;
+
+     printf("Enter number:");
+     scanf("%d",&iValue);
+
+    NonFactors(iValue);
+
+    return 0;
+   }
