6f9568e509d1e3c13598e1e7ee05d86cdf44179a
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-08-19 22:11:05 +0530

    Check if the Number is Positive,Negative or Zero

---
+++ Assignment5/program4.c
@@ -0,0 +1,32 @@
+
+// Check if the Number is Positive,Negative or Zero
+
+#include<stdio.h>
+
+void CheckNumberType(int iNo)
+{
+    if(iNo > 0)
+    {
+        printf("Positive Number");
+    }
+    else if (iNo < 0)
+    {
+        printf("Negative Number");
+    }
+    else if (iNo == 0)
+    {
+        printf("Number is Zero");
+    }
+}
+
+int main()
+{
+ int iValue = 0;
+
+ printf("Enter the Number");
+ scanf("%d",&iValue);
+
+    CheckNumberType(iValue);
+
+    return 0;
+}
