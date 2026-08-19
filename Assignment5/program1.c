811b71d4eca14e6d27a91ef0e0b3d0aab850ea21
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-08-19 22:06:56 +0530

    Check if the Number is Even or Odd

---
+++ Assignment5/program1.c
@@ -0,0 +1,28 @@
+
+// Check if the Number is Even or Odd
+
+#include<stdio.h>
+
+void CheckEvenOdd(int iNo)
+{
+    if(iNo % 2 == 0)
+    {
+        printf("Number is Even");
+    }
+    else
+    {
+        printf("Number is Odd");
+    }
+}
+
+int main()
+{
+   int iValue = 0;
+
+   printf("Enter the Number:");
+   scanf("%d",&iValue);
+
+   CheckEvenOdd(iValue);
+   
+   return 0;
+}
