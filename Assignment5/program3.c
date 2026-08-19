8e1eef1d31551feef4e4115afaefa526102773fe
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-08-19 22:09:54 +0530

    check if the Year is LeapYear or Normal

---
+++ Assignment5/program3.c
@@ -0,0 +1,28 @@
+
+// WAP to check if the Year is LeapYear or Normal
+
+#include<stdio.h>
+
+void CheckLeapYear(int iYear)
+{
+    if(iYear % 4 == 0 || iYear % 400 == 0)
+    {
+        printf("Leap Year");
+    }
+    else
+    {
+        printf("Normal Year");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+ 
+    printf("Enter The Year");
+    scanf("%d",&iValue);
+
+    CheckLeapYear(iValue);
+
+    return 0;
+}
