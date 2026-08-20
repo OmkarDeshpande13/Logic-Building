22bef20504733771c87d8ebd317ca8617ba3c079
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-08-20 23:50:16 +0530

    WAP to demonstate String

---
+++ Assignment6/program1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main()
+{
+   char sName[30];
+
+   printf("Please Enter Your Full Name : ");
+   scanf("%s ",&sName);
+
+   printf("Your name is %s",sName);
+
+    return 0;
+}
