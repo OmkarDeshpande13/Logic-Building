81539cf2024c6896b31b61f422b2a2426332a179
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-08-22 18:48:18 +0530

    WAP to Display the NumberLine

---
+++ Assignment7/program3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+ void DisplayNumberLine(int iNo)
+ {
+    int iCnt;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = -iNo ;iCnt <= iNo; iCnt++)
+    {
+        printf("%d ",iCnt );
+    }
+ }
+
+int main()
+{
+  int iValue = 0;
+
+  printf("Enter the Number: ");
+  scanf("%d",&iValue);
+
+  DisplayNumberLine(iValue);
+
+    return 0;
+}
