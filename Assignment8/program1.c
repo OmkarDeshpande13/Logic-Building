aea62d8e129730852653712661dfe36dbaf9d7dc
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-08-24 22:30:25 +0530

    WAP to Check the Number is Less than or Greater

---
+++ Assignment8/program1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+ void NumberLessThanGreaterThan(int iNo)
+ {
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    if(iNo < 50 )
+    {
+        printf("Number is Small");
+    }
+    else if(iNo > 50 && iNo < 100)
+    {
+        printf("Number is Medium");
+    }
+    else if(iNo > 100)
+    {
+        printf("Number is Greater");
+    }
+ }
+
+int main()
+{
+  int iValue = 0;
+
+  printf("Enter the Number: ");
+  scanf("%d",&iValue);
+
+  NumberLessThanGreaterThan(iValue);
+
+    return 0;
+}
