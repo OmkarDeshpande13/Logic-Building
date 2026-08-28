a71c503a7ea0722c4c07996bb151a43f338b78f1
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-08-28 23:15:26 +0530

    Print all even numbers up to N

---
+++ Assignment13/program2.c
@@ -0,0 +1,30 @@
+
+// Print all even numbers up to N
+
+#include <stdio.h>
+
+void Print_Numbers(int Limit)
+{
+    int i = 0;
+
+    for(i = 1; i <= Limit; i++)
+    {
+        if(i % 2 == 0)
+        {
+            printf("%d\n",i);
+
+        }
+    }
+}
+
+int main()
+{
+    int Value = 0;
+
+    printf("Enter the Value of N : ");
+    scanf("%d",&Value);
+
+    Print_Numbers(Value);
+
+    return 0;
+}
