0162d2aa21a4a3037cc9eaa707d86c172db74a05
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-08-29 23:49:39 +0530

    Accept a Number Count Frequency of 2

---
+++ Assignment14/program3.c
@@ -0,0 +1,41 @@
+/*
+3.Write a program which accept number from user and count frequency of 2 in it.
+Input  :    2395
+Output :    1
+*/
+#include <stdio.h>
+
+int Digits(int iValue)
+{
+    int iDigit = 0;
+    int iFreq = 0;
+
+        while(iValue != 0)
+        {
+            iDigit = iValue % 10;
+            iValue = iValue / 10;
+
+            if(iDigit == 2)
+            {
+                iFreq = iFreq + 1;
+            }
+        }
+        
+
+        return iFreq;
+
+
+}
+
+int main()
+{
+    int iValue = 0,iRet = 0;
+
+    printf("Enter the Number : ");
+    scanf("%d",&iValue);
+
+    iRet = Digits(iValue);
+    printf("Frequency of 2 in the Number is : %d\n",iRet);
+    
+    return 0;
+}
