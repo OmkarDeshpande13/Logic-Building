f08e5ae77c5aab7bffc14c1830584bf488223b3b
OmkarDeshpande13 <omkardeshpande59@gmail.com>
2026-08-29 23:51:05 +0530

    Accept a Number, Count Frequency of 4

---
+++ Assignment14/program4.c
@@ -0,0 +1,43 @@
+/*
+4.Write a program which accept number from user and count frequency of 4 in it.'
+
+Input  :    2395
+Output :    0
+
+*/
+#include <stdio.h>
+
+int Digits(int iValue)
+{
+    int iDigit = 0;
+    int iFreq = 0;
+
+        while(iValue != 0)
+        {
+            iDigit = iValue % 10;
+            iValue = iValue / 10;
+
+            if(iDigit == 4)
+            {
+                iFreq = iFreq + 1;
+            }
+        }
+        
+
+        return iFreq;
+
+
+}
+
+int main()
+{
+    int iValue = 0,iRet = 0;
+
+    printf("Enter the Number : ");
+    scanf("%d",&iValue);
+
+    iRet = Digits(iValue);
+    printf("Frequency of 4 in the Number is : %d\n",iRet);
+    
+    return 0;
+}
